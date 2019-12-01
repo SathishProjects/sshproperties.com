754a31bcff71d1b350f825298adbe21ca01cab9f
sathish43 <rsathishkumar4@gmail.com>
2019-12-01 10:31:47 +0530

    mail template edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,6 +39,8 @@ $(document).ready(function () {
 
     $("#contact-us-form").on('submit', function(event){
         event.preventDefault();
+        $(".submit-form-btn").attr('disabled', true);
+        $('#response').html(`<span class="text-success" style="padding: 20px 0px;"><i class="fa fa-spin fa-spinner"></i> Submitting your query ... Please wait! </span>`);
         var data = {};
         $("#contact-us-form :input").each(function(elem){
             if($(this)[0].name){
@@ -56,10 +58,12 @@ $(document).ready(function () {
                 $('#response').html(`<span class="text-success" style="padding: 20px 0px;"><strong>Your message has been sent. We'll contact you shortly!</strong></span>`);
                 //clear all fields
                 $('#contact-us-form').trigger("reset");
+                $(".submit-form-btn").attr('disabled', false);
             },
             error: function(response) {
                 // Fail message
                 $('#response').html(`<span class="text-danger" style="padding: 20px 0px;"><strong><strong>Sorry ! Server error, try again or reach us in whatsapp <br>@ <a class="text-danger" href="https://wa.me/+919047155511/?text=hi+there+would+like+to+hear+more+about+ssh+properties" target="_blank">+919047155511</a> </strong></span>`);
+                $(".submit-form-btn").attr('disabled', false);
             },
         });
 
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,17 +16,18 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $type = strip_tags(htmlspecialchars($_POST['type']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
+$type2 = strtoupper($type);
+$name2 = strtoupper($name);
 
-$to = 'rsathishkumar4@gmail.com'; 
-$email_subject = "SSH PROPERTIES - QUERY FROM $type - $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\n\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nType: $type\n\nMessage:\n$message";
+$to = $email_address; 
+$email_subject = "SSH PROPERTIES - CUSTOMER INQUIRY - ON $type2 - $name2";
+$email_body = "\n\nHi There,\n\n\nYou have received a new message from sshproperties.com inquiry form.\n\n"."Here are the details:\n\n\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nType: $type\n\nMessage:\n\n----------------------------------------------------\n\n$message\n\n----------------------------------------------------\n\n\n\n-\n\nSSH PROPERTIES";
 $headers = "From: enquiry@sshproperties.com\n"; 
 
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 
-mail($email_address, "SSH PROPERTIES ACKNOWLEDGEMENT" , "Thanks for your intrest in SSH PROPERTIES ! \n\n We will respond to you shortly.", $headers);
+mail($email_address, "SSH PROPERTIES - ONLINE INQUIRY ACKNOWLEDGEMENT" , "\n\n\nHi $name,\n\n Thanks for your intrest in SSH PROPERTIES ! \n\n We will respond to you shortly.\n\n\n\n-\n\nSSH PROPERTIES", $headers);
 
 return true;         
 ?>
