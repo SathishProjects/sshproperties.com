db245fded5885d497f96cfde341a7b1b94c11960
sathish43 <rsathishkumar4@gmail.com>
2019-12-30 07:54:45 +0530

    mail details update

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,10 +19,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $type2 = strtoupper($type);
 $name2 = strtoupper($name);
 
-$to = $email_address; 
+$to = "enquiry@sshproperties.com"; 
 $email_subject = "SSH PROPERTIES - CUSTOMER INQUIRY - ON $type2 - $name2";
 $email_body = "\n\nHi There,\n\n\nYou have received a new message from sshproperties.com inquiry form.\n\n"."Here are the details:\n\n\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nType: $type\n\nMessage:\n\n----------------------------------------------------\n\n$message\n\n----------------------------------------------------\n\n\n\n-\n\nSSH PROPERTIES";
-$headers = "From: enquiry@sshproperties.com\n"; 
+$headers = "From: info@sshproperties.com\n"; 
 
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
